89a7e75ab9759753623e9d97d47f494bd9bb18b5
Brooke Griffin <brookegriff@gmail.com>
2012-04-08 20:51:39 -0400

    Initial Commit

---
+++ project3Tests/project3Tests.h
@@ -0,0 +1,13 @@
+//
+//  project3Tests.h
+//  project3Tests
+//
+//  Created by Brooke Griffin on 4/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface project3Tests : SenTestCase
+
+@end
